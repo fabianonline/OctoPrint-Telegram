e1f4836dfc156e31f00db04ac6331db651ea5e32
derpicknicker1 <10minutemail@web.de>
2016-09-26 03:16:08 +0200

    a bit of cleanup

--- octoprint_telegram/__init__.py
+++ octoprint_telegram/__init__.py
@@ -502,6 +502,13 @@ class TelegramPlugin(octoprint.plugin.EventHandlerPlugin,
 
 	def get_settings_version(self):
 		return 3
+		# Settings version numbers used in releases
+		# < 1.3.0: no settings versioning
+		# 1.3.0 : 1
+		# 1.3.1 : 2
+		# 1.3.2 : 2
+		# 1.3.3 : 2
+		# 1.4.0 : 3
 
 	def get_settings_defaults(self):
 		return dict(
