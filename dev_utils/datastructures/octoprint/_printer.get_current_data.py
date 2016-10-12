0ac7d86bf776622a7410b5eb8988893207e23819
derpicknicker1 <10minutemail@web.de>
2016-10-12 22:05:50 +0200

    New dev datastructures

---
+++ dev_utils/datastructures/octoprint/_printer.get_current_data.py
@@ -0,0 +1,36 @@
+ {
+ 	'progress': {
+ 		'completion': None, 
+ 		'printTime': None, 
+ 		'filepos': None, 
+ 		'printTimeLeft': None
+ 	}, 
+ 	'state': {
+ 		'text': 'Operational', 
+ 		'flags': {
+ 			'operational': True, 
+ 			'paused': False, 
+ 			'printing': False, 
+ 			'sdReady': True, 
+ 			'error': False, 
+ 			'ready': True, 
+ 			'closedOrError': False
+ 		}
+ 	}, 
+ 	'offsets': {}, 
+ 	'job': {
+ 		'estimatedPrintTime': None, 
+ 		'filament': {
+ 			'volume': None, 
+ 			'length': None
+ 		}, 
+	 	'file': {
+	 		'origin': None, 
+	 		'date': None, 
+	 		'name': None, 
+	 		'size': None
+	 	}, 
+ 		'lastPrintTime': None
+ 	}, 
+ 	'currentZ': None
+ }
