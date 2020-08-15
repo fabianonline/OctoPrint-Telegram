4a5d1819a8c75730c6415ba26c9066e163537bc3
giloser <12090417+giloser@users.noreply.github.com>
2020-08-15 12:15:31 +0200

    Update setup.py
    update version number after commit several PR

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_telegram"
 plugin_name = "OctoPrint-Telegram"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.5.1"
+plugin_version = "1.5.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
