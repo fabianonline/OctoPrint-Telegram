081b3a08d6d92165edb0e450703a7ba19a93a457
Francisco de Maussion <franciscodemaussion@gmail.com>
2021-06-04 16:25:08 -0300

    makes use of black

--- dev_utils/datastructures/octoprint/_printer.get_current_connection.py
+++ dev_utils/datastructures/octoprint/_printer.get_current_connection.py
@@ -1,42 +1,26 @@
 (
-	'Operational', 
-	'VIRTUAL', 
-	0, 
-	{
-		'name': 'FlowPrintr', 
-		'color': 'black', 
-		'axes': {
-			'y': {
-				'speed': 9000, 
-				'inverted': False
-			}, 
-			'x': {
-				'speed': 9000, 
-				'inverted': False
-			}, 
-			'z': {
-				'speed': 200, 
-				'inverted': False
-			}, 
-			'e': {
-				'speed': 300, 
-				'inverted': False
-			}
-		}, 
-		'heatedBed': True, 
-		'volume': {
-			'origin': 'lowerleft', 
-			'formFactor': 'rectangular', 
-			'depth': 280.0, 
-			'width': 280.0, 
-			'height': 280.0
-		}, 
-		'model': 'Generic RepRap Printer', 
-		'id': '_default', 
-		'extruder': {
-			'count': 1, 
-			'nozzleDiameter': 0.4, 
-			'offsets': [(0.0, 0.0)]
-		}
-	}
+    "Operational",
+    "VIRTUAL",
+    0,
+    {
+        "name": "FlowPrintr",
+        "color": "black",
+        "axes": {
+            "y": {"speed": 9000, "inverted": False},
+            "x": {"speed": 9000, "inverted": False},
+            "z": {"speed": 200, "inverted": False},
+            "e": {"speed": 300, "inverted": False},
+        },
+        "heatedBed": True,
+        "volume": {
+            "origin": "lowerleft",
+            "formFactor": "rectangular",
+            "depth": 280.0,
+            "width": 280.0,
+            "height": 280.0,
+        },
+        "model": "Generic RepRap Printer",
+        "id": "_default",
+        "extruder": {"count": 1, "nozzleDiameter": 0.4, "offsets": [(0.0, 0.0)]},
+    },
 )
--- dev_utils/testBot/settings_example.py
+++ dev_utils/testBot/settings_example.py
@@ -1,31 +1,34 @@
 # rename this file to settings.py when you are done with setup in this file
-from telegramNotifications import telegramMsgDict # dict of known notification messages
+from telegramNotifications import telegramMsgDict  # dict of known notification messages
+
 settingsDict = {
-	'token': "1234567890:xxXXxx1xXxX1x11x1xXXXxXXXXXXxXXXX!", # <===== insert your bot token here
-	'notification_height' : 5.0,
-	'notification_time' : 15,
-	'message_at_print_done_delay' : 0,
-	'messages' : telegramMsgDict,
-	'tracking_activated' : False,
-	'tracking_token' : None,
-	'debug' : True,
-	'send_icon' : True,
-	'chats' : {
-		'1234567890':{ # <===== insert user account id here
-			'private' : True,
-			'allow_user' : True,
-			'accept_commands' : True,
-			'send_notifications' : True,
-			'title' : 'ExampleUser',
-			'commands':{  # commands dict is only needed if you want to deactivate...
-				'/print': False # ...a command (like /print here). By default they are ALL activated
-			},
-			# do the same for notifications if you want
-			'notifications':{
-				'PrinterStarted': False
-			}
-		},
-		# insert as many users you want before zBOTTOMOFCHATS
-		'zBOTTOMOFCHATS':{'send_notifications': False,'accept_commands':False,'private':False}
-	}
+    "token": "1234567890:xxXXxx1xXxX1x11x1xXXXxXXXXXXxXXXX!",  # <===== insert your bot token here
+    "notification_height": 5.0,
+    "notification_time": 15,
+    "message_at_print_done_delay": 0,
+    "messages": telegramMsgDict,
+    "tracking_activated": False,
+    "tracking_token": None,
+    "debug": True,
+    "send_icon": True,
+    "chats": {
+        "1234567890": {  # <===== insert user account id here
+            "private": True,
+            "allow_user": True,
+            "accept_commands": True,
+            "send_notifications": True,
+            "title": "ExampleUser",
+            "commands": {  # commands dict is only needed if you want to deactivate...
+                "/print": False  # ...a command (like /print here). By default they are ALL activated
+            },
+            # do the same for notifications if you want
+            "notifications": {"PrinterStarted": False},
+        },
+        # insert as many users you want before zBOTTOMOFCHATS
+        "zBOTTOMOFCHATS": {
+            "send_notifications": False,
+            "accept_commands": False,
+            "private": False,
+        },
+    },
 }
